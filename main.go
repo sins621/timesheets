46af8075c23d06224012fe68c6cbe3a9c93658e1
sins <sinsmailza@gmail.com>
2025-08-22 23:35:37 +0200

    chore: simplifiy

--- main.go
+++ main.go
@@ -4,109 +4,36 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
+	"os"
 )
 
-type User struct {
-	Email    string
-	Password string
-}
-
 func main() {
+	const warpBaseUrl = "https://office.warpdevelopment.com"
 
-}
-
-func get(url string) (string, error) {
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-
-	if err != nil {
-		return "could not make request", err
+	type User struct {
+		Email    string
+		Password string
 	}
 
-	resp, err := http.DefaultClient.Do(req)
+	user := User{os.Getenv("email"), os.Getenv("password")}
+	signInData := map[string]string{"Email": user.Email, "Password": user.Password}
+	signInBody, err := json.Marshal(signInData)
 
 	if err != nil {
-		return "error making http request", err
+		fmt.Printf("Error marshalling sign in body: %s", err)
 	}
-	defer resp.Body.Close()
 
-	resBody, err := io.ReadAll(resp.Body)
+	req, err := http.NewRequest(http.MethodPost, warpBaseUrl+"api/account/Authorise", bytes.NewReader(signInBody))
 
 	if err != nil {
-		return fmt.Sprintf("could not read response body: %s\n", err), err
+		fmt.Printf("Error making request object")
 	}
 
-	return fmt.Sprintf("response body: %s\n", resBody), nil
-}
-
-func getWithToken(url string, token string) (string, error) {
-	req, err := http.NewRequest(http.MethodGet, url, nil)
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "Bearer "+token)
 
 	if err != nil {
-		return "could not make request", err
+		fmt.Printf("Error making http request: %s", err)
+		os.Exit(1)
 	}
-
-	resp, err := http.DefaultClient.Do(req)
-
-	if err != nil {
-		return "error making http request", err
-	}
-	defer resp.Body.Close()
-
-	resBody, err := io.ReadAll(resp.Body)
-
-	if err != nil {
-		return fmt.Sprintf("could not read response body: %s\n", err), err
-	}
-
-	return fmt.Sprintf("response body: %s\n", resBody), nil
-}
-
-func post(url string, data map[string]string) (string, error) {
-	jsonData, err := json.Marshal(data)
-
-	if err != nil {
-		return "Error Converting Data into Json", err
-	}
-
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
-
-	resp, err := http.DefaultClient.Do(req)
-	resp.Body.Close()
-
-	statusCode := resp.StatusCode
-	resBody, err := io.ReadAll(resp.Body)
-
-	if err != nil {
-		return fmt.Sprintf("could not read response body: %s\n", err), err
-	}
-
-	return fmt.Sprintf("response body: %s\n status code: %d\n", resBody, statusCode), nil
-}
-
-func postWithToken(url string, token string, data map[string]string) (string, error) {
-	jsonData, err := json.Marshal(data)
-
-	if err != nil {
-		return "Error Converting Data into Json", err
-	}
-
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "Bearer "+token)
-
-	resp, err := http.DefaultClient.Do(req)
-	resp.Body.Close()
-
-	statusCode := resp.StatusCode
-	resBody, err := io.ReadAll(resp.Body)
-
-	if err != nil {
-		return fmt.Sprintf("could not read response body: %s\n", err), err
-	}
-
-	return fmt.Sprintf("response body: %s\n status code: %d\n", resBody, statusCode), nil
 }
