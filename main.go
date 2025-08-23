ea85bc4a3422cc8a3ea644963a38080cd161faea
sins <sinsmailza@gmail.com>
2025-08-23 21:38:16 +0200

    feat: working go

--- main.go
+++ main.go
@@ -16,24 +16,46 @@ func main() {
 		Password string
 	}
 
+	type Reponse struct {
+		Token string `json:"token"`
+	}
+
 	user := User{os.Getenv("email"), os.Getenv("password")}
 	signInData := map[string]string{"Email": user.Email, "Password": user.Password}
 	signInBody, err := json.Marshal(signInData)
 
 	if err != nil {
-		fmt.Printf("Error marshalling sign in body: %s", err)
+		panic(fmt.Sprintf("Error marshalling sign in body: %s", err))
 	}
 
-	req, err := http.NewRequest(http.MethodPost, warpBaseUrl+"api/account/Authorise", bytes.NewReader(signInBody))
+	req, err := http.NewRequest(http.MethodPost, warpBaseUrl+"/api/account/Authorise", bytes.NewReader(signInBody))
 
 	if err != nil {
-		fmt.Printf("Error making request object")
+		panic(fmt.Sprintf("Error making request object: %s", err))
 	}
 
 	req.Header.Set("Content-Type", "application/json")
 
+	client := &http.Client{}
+	res, err := client.Do(req)
+
 	if err != nil {
-		fmt.Printf("Error making http request: %s", err)
-		os.Exit(1)
+		panic(err)
 	}
+
+	defer res.Body.Close()
+
+	token := &Reponse{}
+
+	derr := json.NewDecoder(res.Body).Decode(token)
+
+	if derr != nil {
+		panic(derr)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		panic(res.Status)
+	}
+
+	fmt.Printf("Token %s", token.Token)
 }
