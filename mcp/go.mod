9b1a4ec09341e663a8bb30fae7038575b653d336
Sins_621 <sinsmailza@gmail.com>
2025-08-29 23:38:16 +0200

    chore: http hello world

--- mcp/go.mod
+++ mcp/go.mod
@@ -1,5 +1,3 @@
 module main.go
 
 go 1.24.6
-
-require github.com/joho/godotenv v1.5.1
--- mcp/main.go
+++ mcp/main.go
@@ -1,68 +1,20 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"net/http"
-	"os"
-
-	"github.com/joho/godotenv"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
 func main() {
-	err := godotenv.Load()
+	http.HandleFunc("/", helloHandler)
+
+	fmt.Println("Server starting on http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		panic("No environment file")
+		fmt.Printf("Error starting server: %v\n", err)
 	}
-
-	const warpBaseUrl = "https://office.warpdevelopment.com"
-
-	type User struct {
-		Email    string
-		Password string
-	}
-
-	type Reponse struct {
-		Token string `json:"token"`
-	}
-
-	user := User{os.Getenv("email"), os.Getenv("password")}
-	signInData := map[string]string{"Email": user.Email, "Password": user.Password}
-	signInBody, err := json.Marshal(signInData)
-
-	if err != nil {
-		panic(fmt.Sprintf("Error marshalling sign in body: %s", err))
-	}
-
-	req, err := http.NewRequest(http.MethodPost, warpBaseUrl+"/api/account/Authorise", bytes.NewReader(signInBody))
-
-	if err != nil {
-		panic(fmt.Sprintf("Error making request object: %s", err))
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
-	res, err := client.Do(req)
-
-	if err != nil {
-		panic(err)
-	}
-
-	defer res.Body.Close()
-
-	token := &Reponse{}
-
-	derr := json.NewDecoder(res.Body).Decode(token)
-
-	if derr != nil {
-		panic(derr)
-	}
-
-	if res.StatusCode != http.StatusOK {
-		panic(res.Status)
-	}
-
-	fmt.Printf("Token %s", token.Token)
 }
