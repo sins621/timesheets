1f2846d77fbd67af1285a70383f3b81661f817c6
sins <sinsmailza@gmail.com>
2025-09-08 01:06:10 +0200

    chore: add tool variables

--- mcp_golang/handlers/tool_handler.go
+++ mcp_golang/handlers/tool_handler.go
@@ -2,10 +2,10 @@ package handlers
 
 import (
 	"context"
+	"time"
 
 	"github.com/mark3labs/mcp-go/mcp"
 )
-
 
 type ToolHandler struct {
 	sh *ServiceHandler
@@ -15,6 +15,30 @@ func NewToolHandler(sh *ServiceHandler) *ToolHandler {
 	return &ToolHandler{sh: sh}
 }
 
-func (th *ToolHandler) LogWork(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+func (th *ToolHandler) LogWork(ctx context.Context, r mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+	description, err := r.RequireString("description")
+	if err != nil {
+		return mcp.NewToolResultError(err.Error()), nil
+	}
+
+	t := time.Now()
+
+	date := r.GetString("date", t.Format("2006-01-02T15:04:05"))
+
+	hours, err := r.RequireInt("hours")
+	if err != nil {
+		return mcp.NewToolResultError(err.Error()), nil
+	}
+
+	projectID, err := r.RequireString("projectID")
+	if err != nil {
+		return mcp.NewToolResultError(err.Error()), nil
+	}
+
+	costCodeID, err := r.RequireString("costCodeID")
+	if err != nil {
+		return mcp.NewToolResultError(err.Error()), nil
+	}
+
 	return mcp.NewToolResultText("Working"), nil
 }
