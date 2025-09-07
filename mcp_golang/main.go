aeaaaca3b5507545a5848538331892644ab5e73f
sins <sinsmailza@gmail.com>
2025-09-08 00:03:51 +0200

    feat: log work tool

--- mcp_golang/main.go
+++ mcp_golang/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/mark3labs/mcp-go/mcp"
 	"github.com/mark3labs/mcp-go/server"
 
+	"ts_mcp/constants"
 	"ts_mcp/database"
 	"ts_mcp/handlers"
 	"ts_mcp/request"
@@ -24,15 +25,31 @@ func main() {
 		server.WithToolCapabilities(false),
 	)
 
-	tool := mcp.NewTool("test",
-		mcp.WithDescription("test"),
-		mcp.WithString("name",
+	logTool := mcp.NewTool("Log work on timesheet.",
+		mcp.WithDescription("Allows for logging work done on timesheets."),
+		mcp.WithString("description",
 			mcp.Required(),
-			mcp.Description("test"),
+			mcp.Description("Detailed Description of work done, gather information from git history if necessary"),
+		),
+		mcp.WithString("date",
+			mcp.Description("Date of the entry with format 'YYYY-MM-DDTHH:MM:SS`, default to today's date if not provided"),
+		),
+		mcp.WithNumber("hours",
+			mcp.Required(),
+			mcp.Description("Hours the user has worked on this entry"),
+		),
+		mcp.WithNumber("projectID",
+			mcp.Required(),
+			mcp.Description("The ID of the project the user is working on."),
+		),
+		mcp.WithString("costCodeID",
+			mcp.Required(),
+			mcp.Description("The relevant Cost Code ID of the work done."),
+			mcp.WithStringEnumItems(constants.CostCodeIDs),
 		),
 	)
 
-	s.AddTool(tool, toolHandler.LogWork)
+	s.AddTool(logTool, toolHandler.LogWork)
 
 	if err := server.ServeStdio(s); err != nil {
 		fmt.Printf("Server error: %v\n", err)
