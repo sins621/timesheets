4b3c3537516b28f8fbc36bd2a3ee2e9e672c9839
sins <sinsmailza@gmail.com>
2025-09-13 19:09:18 +0200

    chore: add logWorkService scaffold

--- mcp_golang/handlers/service_handler.go
+++ mcp_golang/handlers/service_handler.go
@@ -1,6 +1,9 @@
 package handlers
 
-import "ts_mcp/interfaces"
+import (
+	"ts_mcp/interfaces"
+	"ts_mcp/models"
+)
 
 type ServiceHandler struct {
 	db interfaces.Database
@@ -9,4 +12,9 @@ type ServiceHandler struct {
 
 func NewServiceHandler(db interfaces.Database, r interfaces.Request) *ServiceHandler {
 	return &ServiceHandler{db: db, r: r}
+}
+
+func (sh *ServiceHandler) logWorkService (email string, password string, workEntry models.WorkEntry) (err error) {
+	err = sh.r.PostTimeSheetEntry("test", 0, workEntry)
+	return err
 }
