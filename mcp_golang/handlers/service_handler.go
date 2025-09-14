a6ebdab57270ae87b1e512f6a001005edb16ce8f
sins <sinsmailza@gmail.com>
2025-09-14 23:31:49 +0200

    feat: log work service code

--- mcp_golang/handlers/service_handler.go
+++ mcp_golang/handlers/service_handler.go
@@ -1,6 +1,9 @@
 package handlers
 
 import (
+	"fmt"
+	"time"
+
 	"ts_mcp/interfaces"
 	"ts_mcp/models"
 )
@@ -14,7 +17,36 @@ func NewServiceHandler(db interfaces.Database, r interfaces.Request) *ServiceHan
 	return &ServiceHandler{db: db, r: r}
 }
 
-func (sh *ServiceHandler) logWorkService (email string, password string, workEntry models.WorkEntry) (err error) {
-	err = sh.r.PostTimeSheetEntry("test", 0, workEntry)
+func (sh *ServiceHandler) logWorkService(email string, password string, workEntry models.WorkEntry) (err error) {
+	user, err := sh.db.SelectUserByEmail(email)
+
+	if err != nil {
+		token, err := sh.r.GetUserToken(email, password)
+		if err != nil {
+			return fmt.Errorf("error getting user token for email: %s\n, err: %v\n", email, err)
+		}
+
+		personID, err := sh.r.GetPersonID(token)
+		if err != nil {
+			return fmt.Errorf("error getting user person id for email: %s\n, err: %v\n", email, err)
+		}
+
+		user, err = sh.db.CreateUser(&models.User{
+			Email:         email,
+			Token:         token,
+			PersonId:      personID,
+			InitializedAt: time.Now(),
+		})
+	} else if time.Since(user.InitializedAt) > time.Hour*24*7 {
+		token, err := sh.r.GetUserToken(email, password)
+		if err != nil {
+			return fmt.Errorf("error getting user token for email: %s\n, err: %v\n", email, err)
+		}
+
+		user.Token = token
+		user, err = sh.db.UpdateUser(user)
+	}
+
+	err = sh.r.PostTimeSheetEntry(user.Token, user.PersonId, workEntry)
 	return err
 }
